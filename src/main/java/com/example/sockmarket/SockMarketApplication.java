438b953dc077d17ce6d16dcfa5ca633cd810e282
Александр <alexakr1993@gmail.com>
2023-04-08 11:47:25 +0400

    Begin

---
+++ src/main/java/com/example/sockmarket/SockMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.sockmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SockMarketApplication.class, args);
+    }
+
+}
